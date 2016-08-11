3db3531afe64f12c87c5bf1ad4b16e0b018e574b
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2016-08-11 16:40:06 +0100

    Add demonstration of working code to index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
+require 'URLDeconstructor.php';
 
+$url = new URLDeconstructor('http://www.totallymadeupdomain.com/totally/made/up/path');
+
+printf('<p>%s : %s</p>', 'The protocol is', $url->getProtocol());
+printf('<p>%s : %s</p>', 'The domain is', $url->getDomain());
+printf('<p>%s : %s</p>', 'The path is', $url->getPath());
