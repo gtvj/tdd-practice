e808b12a31ac29b7801c84f53a6d99b638e4f0fa
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2016-08-11 18:39:01 +0100

    Add name attribute functionality

--- SubmissionTester.php
+++ SubmissionTester.php
@@ -1,7 +1,22 @@
 <?php
 
-
 class SubmissionTester
 {
+    private $name;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function __construct($name)
+    {
+        $this->setName($name);
+    }
 
 }
--- tests/SubmissionTesterTest.php
+++ tests/SubmissionTesterTest.php
@@ -4,13 +4,20 @@ require dirname(__DIR__) . '/SubmissionTester.php';
 
 class SubmissionTesterTest extends PHPUnit_Framework_TestCase
 {
+    protected function setUp() {
+
+        $bad_words = array('onions', 'pepperoni');
+        $this->submission = new SubmissionTester('Gwyn', 'More onions, pepperoni, cheese and mushrooms for me please',
+            $bad_words);
+    }
 
     public function testInstanceOf()
     {
-        $bad_words = array('onions', 'pepperoni');
-        $submission = new SubmissionTester('Gwyn', 'More onions, pepperoni, cheese and mushrooms for me please',
-            $bad_words);
+        $this->assertInstanceOf('SubmissionTester', $this->submission, "It is an instance of the SubmissionTester class");
+    }
 
-        $this->assertInstanceOf('SubmissionTester', $submission, "It is an instance of the SubmissionTester class");
+    public function testNameAttribute()
+    {
+        $this->assertEquals($this->submission->getName(), 'Gwyn');
     }
 }
