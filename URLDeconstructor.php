2b0e926ba473b99ff12f98d6d5a779498f9ebab6
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2016-08-11 15:47:44 +0100

    Add functionality to extract path

--- URLDeconstructor.php
+++ URLDeconstructor.php
@@ -3,5 +3,25 @@
 
 class URLDeconstructor
 {
+    private $url;
+    private $path;
+    private $pattern = '/((http)s?:\/\/)?(www.)?[^\.]*.(gov.uk|com|co.uk)(\/.*)/';
+
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    public function setPath($url)
+    {
+        $this->path = preg_filter($this->pattern, '$5', $url);
+    }
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+        $this->setPath($this->url);
+    }
+
 
 }
--- tests/URLDeconstructorTest.php
+++ tests/URLDeconstructorTest.php
@@ -7,8 +7,22 @@ class URLDeconstructorTest extends PHPUnit_Framework_TestCase
 
     public function testURLDeconstructorType()
     {
-        $url = new URLDeconstructor();
+        $url = new URLDeconstructor('http://www.nationalarchives.gov.uk/news');
         $this->assertInstanceOf('URLDeconstructor', $url, "The object is an instance of URLDeconstructor");
     }
-    
+
+    public function testPathAttribute()
+    {
+        $url = new URLDeconstructor('http://www.nationalarchives.gov.uk/news');
+        $this->assertEquals($url->getPath(), '/news', "The path attribute was set successfully");
+
+        $url = new URLDeconstructor('http://www.google.com/news');
+        $this->assertEquals($url->getPath(), '/news', "The path attribute was set successfully");
+
+        $url = new URLDeconstructor('https://www.example.co.uk/news');
+        $this->assertEquals($url->getPath(), '/news', "The path attribute was set successfully");
+
+    }
+
+
 }
